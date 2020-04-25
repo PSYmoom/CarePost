a7cbe66683cd27547e03c639000ed48084db0b59
Mazharul Maaz <maazmazharul02@gmail.com>
2020-04-25 10:27:05 -0400

    Initial commit

---
+++ app/src/main/java/com/carepost/MainActivity.java
@@ -0,0 +1,35 @@
+package com.carepost;
+import java.util.ArrayList;
+import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addMore = findViewById(R.id.addMore);
+        Button submitButton = findViewById(R.id.submitButton);
+
+        addMore.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                List<String> subList = new ArrayList<>();
+                List<List<String>> infoList = new ArrayList<>();
+                EditText frstName = findViewById(R.id.frstName);
+                EditText lastName = findViewById(R.id.lastName);
+                EditText aptNum = findViewById(R.id.aptNum);
+                subList.add(frstName.getText().toString());
+                subList.add(lastName.getText().toString());
+                subList.add(aptNum.getText().toString());
+                infoList.add(subList);
+            }
+        });
+    }
+}
